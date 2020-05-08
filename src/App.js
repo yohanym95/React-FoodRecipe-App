0e75cef5155d943a505ee09ab0e86a7c488003c1
yohanym95 <malshikay@gmail.com>
2020-05-08 12:27:01 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Recipe from './components/recipe'
+
+function App () {
+  return (
+    <div className='App'>
+      <Recipe />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/recipe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Recipe">
+      <p> Hello React Lovers</p>
+    </div>
+  );
+}
+
+export default App;
